be5c0b646cac15870155a3e95b0beb01c9d8155e
Lucio <luciocienfuegos41@gmail.com>
2025-01-15 18:51:35 -0300

    registro

--- components/registro.js
+++ components/registro.js
@@ -39,7 +39,7 @@ function CrearBotonRegistrosEjercicios(contenedorRegistro) {
 }
 
 function MostrarRegistroEjercicios(contenedorRegistro){
-    window.location.href = 'http://127.0.0.1:5500/pages/registros.html'
+    window.location.href = './pages/registros.html'
 }
 
 function CrearbotonVolverAtras(seccionDias,seccionRutinas, overflow){
